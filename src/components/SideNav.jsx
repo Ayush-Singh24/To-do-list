b15728f5ee7fa18f6fd8f1e600e5b836e80889db
Ayush Singh <asayushsingh80@gmail.com>
2023-06-26 01:12:59 +0530

    fixed bug in navigation

--- src/components/SideNav.jsx
+++ src/components/SideNav.jsx
@@ -3,43 +3,43 @@ import { Options } from "../utils/constants";
 export default function SideNav({ changeOption, option }) {
   return (
     <nav className="side-nav">
-      <div className={option === Options.All ? "active" : ""}>
+      <div
+        className={option === Options.All ? "active" : ""}
+        onClick={() => changeOption(Options.All)}
+      >
         <img
           src="/assets/all-task-icon.svg"
           alt="all-task"
           className="side-nav__img"
         />
-        <p className="side-nav__type" onClick={() => changeOption(Options.All)}>
-          All tasks
-        </p>
+        <p className="side-nav__type">All tasks</p>
       </div>
-      <div className={option === Options.High ? "active" : ""}>
+      <div
+        className={option === Options.High ? "active" : ""}
+        onClick={() => changeOption(Options.High)}
+      >
         <img
           src="/assets/high-priority-icon.svg"
           alt="high priority"
           className="side-nav__img"
         />
-        <p
-          className="side-nav__type"
-          onClick={() => changeOption(Options.High)}
-        >
-          High Priority
-        </p>
+        <p className="side-nav__type">High Priority</p>
       </div>
-      <div className={option === Options.Medium ? "active" : ""}>
+      <div
+        className={option === Options.Medium ? "active" : ""}
+        onClick={() => changeOption(Options.Medium)}
+      >
         <img
           src="/assets/medium-priority-icon.svg"
           alt="medium priority"
           className="side-nav__img"
         />
-        <p
-          className="side-nav__type"
-          onClick={() => changeOption(Options.Medium)}
-        >
-          Medium Priority
-        </p>
+        <p className="side-nav__type">Medium Priority</p>
       </div>
-      <div className={option === Options.Low ? "active" : ""}>
+      <div
+        className={option === Options.Low ? "active" : ""}
+        onClick={() => changeOption(Options.Low)}
+      >
         <div>
           <img
             src="/assets/low-priority-icon.svg"
@@ -47,11 +47,12 @@ export default function SideNav({ changeOption, option }) {
             className="side-nav__img"
           />
         </div>
-        <p className="side-nav__type" onClick={() => changeOption(Options.Low)}>
-          Low Priority
-        </p>
+        <p className="side-nav__type">Low Priority</p>
       </div>
-      <div className={option === Options.No ? "active" : ""}>
+      <div
+        className={option === Options.No ? "active" : ""}
+        onClick={() => changeOption(Options.No)}
+      >
         <div>
           <img
             src="/assets/no-priority-icon.svg"
@@ -59,22 +60,18 @@ export default function SideNav({ changeOption, option }) {
             className="side-nav__img"
           />
         </div>
-        <p className="side-nav__type" onClick={() => changeOption(Options.No)}>
-          No Priority
-        </p>
+        <p className="side-nav__type">No Priority</p>
       </div>
-      <div className={option === Options.Completed ? "active" : ""}>
+      <div
+        className={option === Options.Completed ? "active" : ""}
+        onClick={() => changeOption(Options.Completed)}
+      >
         <img
           src="/assets/completed-icon.svg"
           alt="completed"
           className="side-nav__img"
         />
-        <p
-          className="side-nav__type"
-          onClick={() => changeOption(Options.Completed)}
-        >
-          Completed
-        </p>
+        <p className="side-nav__type">Completed</p>
       </div>
     </nav>
   );
